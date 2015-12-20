27097697508c64f8e05605775063b07dc771745a
Cory Mawhorter <cory.mawhorter@gmail.com>
2015-12-19 17:26:50 -0800

    no write on dev

--- app/lib/logger.js
+++ app/lib/logger.js
@@ -2,6 +2,13 @@
 
 var fs = require('fs');
 
+var IS_DEV = process.env.NODE_ENV === 'development';
+
 module.exports = function log(entry) {
-  fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+  if (IS_DEV) {
+    console.log(entry);
+  }
+  else {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+  }
 };
